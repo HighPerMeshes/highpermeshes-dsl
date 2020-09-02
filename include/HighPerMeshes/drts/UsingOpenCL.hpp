d527ba3174db2f5b4fcd5af5bd0a746d7bd73f69
Paolo Gorlani <paogor@users.noreply.github.com>
2020-09-02 10:58:12 +0200

    added default queue and context

--- include/HighPerMeshes/drts/UsingOpenCL.hpp
+++ include/HighPerMeshes/drts/UsingOpenCL.hpp
@@ -10,6 +10,7 @@
 #include <HighPerMeshes/dsl/dispatchers/OpenCLDispatcher.hpp>
 
 #define CL_HPP_ENABLE_EXCEPTIONS
+#define CL_HPP_MINIMUM_OPENCL_VERSION 110
 #define CL_HPP_TARGET_OPENCL_VERSION 200
 #include <CL/cl2.hpp>
 
@@ -56,7 +57,7 @@ namespace HPM
             std::cout<<"Platform: "<<platform.getInfo<CL_PLATFORM_NAME>()<<std::endl;
           
             // select device/s
-            platform.getDevices(CL_DEVICE_TYPE_ACCELERATOR, &devices);
+            platform.getDevices(CL_DEVICE_TYPE_DEFAULT, &devices);
 
             for(auto dev = devices.begin(); dev != devices.end(); )
                 if(dev->getInfo<CL_DEVICE_NAME>().find(cl_device_name) == std::string::npos)
@@ -64,7 +65,7 @@ namespace HPM
                 else ++dev;
 
             // for now use just a single device
-            if(devices.size() != 1) throw std::runtime_error("ocl device error"); 
+            if(devices.size() < 1) throw std::runtime_error("no ocl devices found"); 
           
             cl::Device device = devices[0];
             std::cout<<"Device: "<<device.getInfo<CL_DEVICE_NAME>()<<std::endl;
@@ -74,16 +75,23 @@ namespace HPM
 
             // create context         
             context = cl::Context(devices);
+            cl::Context defc = cl::Context::setDefault(context);
+            if (defc != context)
+                throw std::runtime_error("error setting default context");
 
             // create default queue 
             default_queue = cl::CommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE);
+            cl::CommandQueue defq = cl::CommandQueue::setDefault(default_queue);
+            if (defq != default_queue)
+                throw std::runtime_error("error setting default queue");
+
 
         }
 
         void LoadKernelsFromBinary(std::string binFilename, std::vector<kernel_name_type> kernelNames)
         {
 
-            std::cerr<<"Loading OpenCL kernel binary from '"<<binFilename<<"' ..."<<std::endl;
+            std::cerr<<"Loading OpenCL kernel binary from '"<<binFilename<<"' ...";
 
             std::ifstream bin_file(binFilename, std::ifstream::binary);
 
@@ -94,7 +102,9 @@ namespace HPM
 
             bin_file.seekg (0, bin_file.beg);
             bin_file.read((char*) buf.data(), bin_file_size);
-          
+            
+            std::cerr<<" DONE"<<std::endl;
+ 
             cl::Program::Binaries bins;
             bins.push_back(buf);
           
@@ -162,11 +172,7 @@ namespace HPM
         template <typename T, typename MeshT, typename DofT>
         void UnmapSVMBuffer(Buffer<T, MeshT, DofT, OpenCLHandler::SVMAllocator<T>> & buffer)
         {
-            try
-            {
-                default_queue.enqueueUnmapSVM((void*) buffer.GetData());
-            }
-            catch (...) {};
+            default_queue.enqueueUnmapSVM((void*) buffer.GetData());
         }
 
         template <typename T, typename MeshT, typename DofT>
